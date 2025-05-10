ffb4ebdd6f53205048330e1da76c7f584b6a8152
Amirreza5859 <amirreza5859@gmail.com>
2025-05-11 02:17:00 +0330

    sell

---
+++ src/main/java/controllers/SellController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import models.Result;
+import models.data.Repository;
+
+public class SellController extends Controller {
+    public SellController(Repository repo) {
+        super(repo);
+    }
+
+    @Override
+    public Result handleCommand(String commandLine) {
+
+    }
+}
