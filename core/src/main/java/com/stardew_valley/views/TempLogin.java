df9aa547f40d91c7f968d05a22b3d93fb0413ff0
KiaValliant <akghasemim@gmail.com>
2025-07-26 05:34:15 +0330

    some assets added

---
+++ core/src/main/java/com/stardew_valley/views/TempLogin.java
@@ -0,0 +1,20 @@
+package com.stardew_valley.views;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class TempLoginView extends View {
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public Stage getStage() {
+        return null;
+    }
+
+    @Override
+    public void show() {
+
+    }
+}
