22fdf6e373e20bf1d6ac0cb9192206574fe4f2bd
Amirreza5859 <amirreza5859@gmail.com>
2025-07-26 23:45:19 +0330

    game menu added!

--- core/src/main/java/com/stardew_valley/Main.java
+++ core/src/main/java/com/stardew_valley/Main.java
@@ -2,18 +2,17 @@ package com.stardew_valley;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.stardew_valley.controllers.GameMenuController;
 import com.stardew_valley.controllers.LoginMenuController;
+import com.stardew_valley.controllers.SignUpMenuController;
 import com.stardew_valley.models.data.Repository;
 import com.stardew_valley.models.data.User;
 import com.stardew_valley.models.enums.Gender;
 import com.stardew_valley.models.enums.SecurityQuestion;
-import com.stardew_valley.views.AppView;
-import com.stardew_valley.views.LoginMenuView;
+import com.stardew_valley.views.*;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.stardew_valley.controllers.GameController;
 import com.stardew_valley.models.data.Repository;
-import com.stardew_valley.views.GameView;
-import com.stardew_valley.views.TempLogin;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
@@ -34,7 +33,8 @@ public class Main extends Game {
         batch = new SpriteBatch();
         repo = new Repository();
         main = this;
-        setScreen(new LoginMenuView(new LoginMenuController(repo)));
+        setScreen(new SignUpMenuView(new SignUpMenuController(repo)));
+        //setScreen(new GameMenuView(new GameMenuController(repo)));
     }
 
     @Override
--- core/src/main/java/com/stardew_valley/views/GameMenuView.java
+++ core/src/main/java/com/stardew_valley/views/GameMenuView.java
@@ -1,24 +1,34 @@
 package com.stardew_valley.views;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.stardew_valley.controllers.GameMenuController;
 import com.stardew_valley.models.AssetManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class GameMenuView extends View {
     private Stage stage;
     private Skin skin;
     private Table table;
+    private Table buttonsTable;
 
-    private Label titleLabel;
+    private SelectBox<String> numOfPlayers;
+    private List<TextField> players;
+    private List<Label> playersLabel;
+    private TextButton startGame;
+    private TextButton nextTurn;
     private TextButton backButton;
+    private Label messageLabel;
 
     private final GameMenuController controller;
 
@@ -26,8 +36,15 @@ public class GameMenuView extends View {
         this.controller = controller;
         this.skin = AssetManager.getAssetManager().getSkin();
 
-        titleLabel = new Label("Fuck", skin);
+        numOfPlayers = new SelectBox<>(skin);
+        numOfPlayers.setItems("2","3","4");
+        players = new ArrayList<>();
+        playersLabel = new ArrayList<>();
+        startGame = new TextButton("Start Game", skin);
+        nextTurn = new TextButton("Next Turn", skin);
         backButton = new TextButton("Back", skin);
+        messageLabel = new Label("", skin);
+
     }
 
     @Override
@@ -36,28 +53,81 @@ public class GameMenuView extends View {
         Gdx.input.setInputProcessor(stage);
 
         table = new Table(skin);
-        table.setFillParent(true);
         table.center();
-        table.add(titleLabel).pad(10).row();
-        table.add(backButton).pad(10).row();
 
-        stage.addActor(table);
+        buttonsTable = new Table(skin);
+        buttonsTable.center();
+        buttonsTable.add(startGame).pad(10).row();
+        buttonsTable.add(nextTurn).pad(10).row();
+        buttonsTable.add(backButton).pad(10).row();
+        buttonsTable.add(messageLabel).pad(10).row();
 
+        Table mainTable = new Table(skin);
+        mainTable.setFillParent(true);
+        mainTable.center();
+        mainTable.add(table).pad(20).row();
+        mainTable.add(buttonsTable);
+        stage.addActor(mainTable);
         handleInput();
     }
 
     @Override
     public void handleInput() {
+
+        startGame.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                controller.startGame(messageLabel,players);
+            }
+        });
+
+        nextTurn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                controller.nextTurn(messageLabel);
+            }
+        });
+
         backButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                controller.back(titleLabel);
+                controller.back(messageLabel);
             }
         });
+
+        numOfPlayers.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                updatePlayerFields(Integer.parseInt(numOfPlayers.getSelected()) - 1);
+            }
+        });
+
+        updatePlayerFields(1);
     }
 
     @Override
     public Stage getStage() {
         return stage;
+    }
+
+    private void updatePlayerFields(int count) {
+        table.clear();
+
+        table.add("num of players:").pad(10);
+        table.add(numOfPlayers).pad(10).row();
+
+        players.clear();
+        playersLabel.clear();
+
+        for (int i = 1; i <= count; i++) {
+            Label label = new Label("Player " + i + " Username:", skin);
+            TextField textField = new TextField("", skin);
+            players.add(textField);
+            playersLabel.add(label);
+
+            table.row();
+            table.add(label).pad(5);
+            table.add(textField).width(200).pad(5);
+        }
     }
 }
