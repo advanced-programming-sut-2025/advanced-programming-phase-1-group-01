bc4122a65f838a52857b1e652ca4e769fc3ecde1
KiaValliant <akghasemim@gmial.com>
2025-08-05 20:53:48 +0330

    notifications view added

--- core/src/main/java/com/stardew_valley/views/GiftView.java
+++ core/src/main/java/com/stardew_valley/views/GiftView.java
@@ -1,13 +1,21 @@
 package com.stardew_valley.views;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Plane;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.stardew_valley.models.AssetManager;
+import com.stardew_valley.models.character.player.Player;
 
 public class GiftView extends GameWindow {
 
+    private Player friend;
+
     public GiftView() {
         super("Gift", AssetManager.getAssetManager().getSkin(), "Letter");
+    }
+
+    public void setFriend(Player friend) {
+        this.friend = friend;
     }
 
     @Override
